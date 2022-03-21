a4b769db28e20c1940ef83014bb4e77e2b13eb77
shiness <scb66695@open-groupe.com>
2022-03-21 15:45:28 +0100

    ajout final app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { Exercice2Component } from './exercice2/exercice2.component';
-import { Exercice1Component } from './exercice1/exercice1.component';
-import { Exercice3Component } from './exercice3/exercice3.component';
-import { Exercice4Component } from './exercice4/exercice4.component';
+import { AppComponent } from "./app.component";
+import { Exercice2Component } from "./exercice2/exercice2.component";
+import { Exercice1Component } from "./exercice1/exercice1.component";
+import { Exercice3Component } from "./exercice3/exercice3.component";
+import { Exercice4Component } from "./exercice4/exercice4.component";
 
 @NgModule({
   declarations: [
@@ -13,12 +13,10 @@ import { Exercice4Component } from './exercice4/exercice4.component';
     Exercice2Component,
     Exercice1Component,
     Exercice3Component,
-    Exercice4Component
+    Exercice4Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
